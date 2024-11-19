162496cbbcc13b859e35e924ec944abfc9e131d7
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-11-19 21:13:01 +0100

    Remove a33x from supported devices for AOD-LK transition

--- customize.sh
+++ customize.sh
@@ -136,7 +136,7 @@ elif [[ "$(getprop ro.product.product.name)" == "a33x"* || "$(getprop ro.product
     tar xvf "$MODPATH/resources/bootanim-1080x2400.tar" -C "$MODPATH/system/media/"
 fi
 
-if [[ "$(getprop ro.product.product.name)" == "a35x"* || "$(getprop ro.product.product.name)" == "a55x"* || "$(getprop ro.product.product.name)" == "a33x"* || "$(getprop ro.product.product.name)" == "a53x"* || "$(getprop ro.product.product.name)" == "a34x"* || "$(getprop ro.product.product.name)" == "a54x"* ]]; then
+if [[ "$(getprop ro.product.product.name)" == "a35x"* || "$(getprop ro.product.product.name)" == "a55x"* || "$(getprop ro.product.product.name)" == "a53x"* || "$(getprop ro.product.product.name)" == "a34x"* || "$(getprop ro.product.product.name)" == "a54x"* ]]; then
    ui_print "- Enabling AOD-Lockscreen clock transition..."
    tar xvzf "$MODPATH/resources/BiometricSettings-s23fe.tar.gz" -C "$MODPATH/system/"
    SET_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_AOD_ITEM" "aodversion=7,clocktransition"
